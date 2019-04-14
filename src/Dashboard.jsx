c92d9e2b79e22f7015732394a7c0c2affdb095be
Sandor Biba <sandorbiba@gmail.com>
2019-04-14 11:04:05 +0200

    add Note + Dashboard

---
+++ src/Dashboard.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Note from './Note';
+import '.Note.css';
+
+notes = [
+    {fsdfsd},
+    {sdfsdf},
+    {sdfsdf}
+]
+
+class Dashboard extends Component {
+  render () {
+    return (
+      <div className='Note'>
+          <Note />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
+
---
+++ src/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Note = notes.map(obj =>
+    <div>
+    return <Note obj={obj} />
+    </div>
+
+);
+  
+export default Note;
+
